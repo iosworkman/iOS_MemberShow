1a3415e3d57dbbccce736cfcad64d0b562f22c62
Weizhou <shiwoow@gmail.com>
2014-05-12 21:11:10 +0800

    branch first commit
    branch first commit

---
+++ iOS_MemberShow/Member/BC_BifidyCAPs/dopcnViewController.h
@@ -0,0 +1,13 @@
+//
+//  dopcnViewController.h
+//  iOS_MemberShow
+//
+//  Created by weizhou on 5/12/14.
+//  Copyright (c) 2014 BifidyCAPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dopcnViewController : UIViewController
+
+@end
